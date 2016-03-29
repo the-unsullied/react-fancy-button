9962dc47f74ab9a15cbf9dad68785cff4d0c4012
Matt Goo <mattgoo@gmail.com>
2016-03-29 16:28:25 -0700

    added css

---
+++ index.js
@@ -0,0 +1,2 @@
+export {default as FancyButton} from './dist/FancyButton';
+export {default as FancyButtonStyles} from './dist/FancyButton.css';
--- scripts/build-dist.js
+++ scripts/build-dist.js
@@ -1,28 +1,14 @@
-var Builder = require('systemjs-builder');
 var sass = require('node-sass');
-var sassJspm = require('sass-jspm-importer');
 var fs = require('fs');
 
 sass.render({
-  file: './src/main.scss',
-  importer: sassJspm.importer
+  file: './src/main.scss'
 }, function(error, result) { // node-style callback from v3.0.0 onwards
   if(!error){
-    fs.writeFile('./dist/main.css', result.css, function(err){
+    fs.writeFile('./dist/FancyButton.css', result.css, function(err){
       if(!err){
-        console.log('file written to disk')
+        console.log('css file written to disk')
       }
     });
   }
 });
-
-
-new Builder('./', './config.js')
-  .bundle('./src/FancyButton', './dist/component.js')
-  .then(() => {
-    console.log('Build complete');
-  })
-  .catch(err => {
-    console.log('Build error');
-    console.log(err);
-  });
